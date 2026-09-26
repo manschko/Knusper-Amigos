f62d90903ce1c49fa51500dde9e5f9f0daf13af7
jennyx21 <jennjenn2101@googlemail.com>
2026-09-26 13:00:19 +0200

    enemyspawner

--- enemy_sparne/en_spaw.gd
+++ enemy_sparne/en_spaw.gd
@@ -1,12 +1,33 @@
 extends Node3D
 
 @export var charatke: CharacterBody3D
-@export var enemy: CharacterBody3D
+@export var enemy: PackedScene
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var timer = 1.0
+var i = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	charatke.position.y
+	timer -= delta
+	if timer <= 0.0: 
+		timer = 5.0
+		i = randi_range(0, 1000)
+		if i <= 950:
+			spawn()
+	
+func spawn() -> void:
+	var z = charatke.position.z
+	var x = charatke.position.x
+	var winkel = randf_range(0.0, TAU)
+	var radius = randf_range(5, 25)
+	var dx = x + cos(winkel) * radius
+	var dz = z + sin(winkel) * radius
+	var spawn_position = Vector3(
+	dx,
+	charatke.position.y,
+	dz)
+	var neuer_enemy = enemy.instantiate()
+	get_tree().current_scene.add_child(neuer_enemy)
+
+	neuer_enemy.global_position = spawn_position
